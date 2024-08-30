38f94214282ade5d83e33d8b3aee9dd32cb3c840
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-30 13:12:23 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom/Switch';
+import { Link } from 'react-router-dom';
 import { getCurrentUser, signOut } from '../services/auth';
 
 function Navbar() {
