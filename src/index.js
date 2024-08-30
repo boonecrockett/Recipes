c158af6a95af15ca61baf1478311fb0977560a3f
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-30 16:18:28 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
 import './styles/index.css';
 import App from './components/App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root'); // Get the root DOM element
+const root = createRoot(rootElement); // Create a root with createRoot
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
