c9c32efefc759fc1655bdd47f253e945dbf7cde2
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-30 12:56:40 -0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/Switch';
 import Navbar from './Navbar';
 import RecipeList from './RecipeList';
 import RecipeDetail from './RecipeDetail';
