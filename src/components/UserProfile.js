9ac98932f7a8ec6f071bdf706157ff06c68da0c6
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-30 13:14:18 -0600

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom/Switch';
+import { Link } from 'react-router-dom';
 import { getCurrentUser } from '../services/auth';
 import { getSpreadSheetValues } from '../services/googleSheets';
 import { formatRating } from '../utils/recipeUtils';
