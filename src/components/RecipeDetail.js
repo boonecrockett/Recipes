e3d6d12164c8523084b52c2f25e24da6e104c2d4
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-30 13:13:33 -0600

    Update RecipeDetail.js

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom/Switch';
+import { useParams } from 'react-router-dom';
 import { getSpreadSheetValues, appendSpreadSheetValues, updateSpreadSheetValues } from '../services/googleSheets';
 import { formatRating } from '../utils/recipeUtils';
 import { getCurrentUser } from '../services/auth';
