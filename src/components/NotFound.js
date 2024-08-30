27f3cb0bc48197359341af731df2b3d0977fc02b
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-30 16:12:26 -0600

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
     <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you're looking for doesn't exist.</p>
-      <Link to="/">Go back to homepage</Link>
+      <h1>Page Not Found</h1>
+      <p>It&apos;s not here!</p> {/* Escaping the single quote */}
     </div>
   );
 }
