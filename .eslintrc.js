501cdf13954ae94507edd490d390fe345f51db97
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-30 16:09:04 -0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,36 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended' // Add recommended hooks rules
+    'plugin:react-hooks/recommended',
   ],
-  plugins: ['react', 'react-hooks'], // Include the hooks plugin
+  plugins: ['react', 'react-hooks'],
   rules: {
     'react-hooks/rules-of-hooks': 'error', // Enforce the rules of hooks
-    'react-hooks/exhaustive-deps': 'warn' // Warn about missing dependencies
+    'react-hooks/exhaustive-deps': 'warn', // Warn about missing dependencies
+    'no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        args: 'none', // Do not warn for unused function arguments
+        ignoreRestSiblings: true, // Ignore unused variables when using rest siblings
+      },
+    ], // Change unused variables to warnings instead of errors
+    'quotes': [
+      'error',
+      'single',
+      { avoidEscape: true }, // Allow single quotes in JSX when escaped
+    ],
+    'react/jsx-uses-react': 'off', // Disable for React 17+ with new JSX transform
+    'react/react-in-jsx-scope': 'off', // Disable for React 17+ with new JSX transform
   },
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
+      version: 'detect',
+    },
+  },
 };
