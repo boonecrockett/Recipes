2705fb240930cabed9a3e842b88b71aca01f552e
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-31 17:05:18 -0600

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,9 +1,13 @@
-// src/utils/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const firebaseConfig = {
-  // Your Firebase configuration here
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -15,31 +19,3 @@ export const signInWithGoogle = () => {
 };
 
 export { auth };
-
-// src/utils/googleSheets.js
-import { GoogleSpreadsheet } from 'google-spreadsheet';
-
-const SPREADSHEET_ID = process.env.REACT_APP_GOOGLE_SHEETS_ID;
-const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_SHEETS_CLIENT_EMAIL;
-const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SHEETS_PRIVATE_KEY;
-
-export const submitRecipe = async (recipe) => {
-  const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
-
-  await doc.useServiceAccountAuth({
-    client_email: CLIENT_EMAIL,
-    private_key: PRIVATE_KEY,
-  });
-
-  await doc.loadInfo();
-  const sheet = doc.sheetsByIndex[0];
-
-  await sheet.addRow({
-    'Recipe Name': recipe.name,
-    'Ingredients': recipe.ingredients,
-    'Preparation Steps': recipe.steps,
-    'Game Type': recipe.gameType,
-    'Cooking Method': recipe.cookingMethod,
-    'Cooking/Preparation Time': recipe.cookingTime,
-  });
-};
