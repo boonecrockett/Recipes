f8098b97109bfbb08914b8818f3cf7f3e168c078
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-31 10:58:33 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';  // This line should be present
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
-
+console.log('React is loading...'); // Add this line
 ReactDOM.render(
   <React.StrictMode>
     <App />
