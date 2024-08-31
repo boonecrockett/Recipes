735a73464157e318a716a0a6490ab7666ec35117
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-31 09:47:36 -0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,39 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+{
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true,
+    "jest": true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended"
   ],
-  plugins: ['react', 'react-hooks'],
-  rules: {
-    'react-hooks/rules-of-hooks': 'error', // Enforce the rules of hooks
-    'react-hooks/exhaustive-deps': 'warn', // Warn about missing dependencies
-    'no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        args: 'none', // Do not warn for unused function arguments
-        ignoreRestSiblings: true, // Ignore unused variables when using rest siblings
-      },
-    ], // Change unused variables to warnings instead of errors
-    'quotes': [
-      'error',
-      'single',
-      { avoidEscape: true }, // Allow single quotes in JSX when escaped
-    ],
-    'react/jsx-uses-react': 'off', // Disable for React 17+ with new JSX transform
-    'react/react-in-jsx-scope': 'off', // Disable for React 17+ with new JSX transform
-  },
-  settings: {
-    react: {
-      version: 'detect',
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
+    "ecmaVersion": 12,
+    "sourceType": "module"
   },
-};
+  "plugins": [
+    "react",
+    "react-hooks",
+    "jsx-a11y"
+  ],
+  "rules": {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    "no-console": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
+}
