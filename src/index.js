928c145c6f6f9770702cd08755490375aeaad0b0
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-31 09:45:01 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
-import './styles/index.css';
-import App from './components/App';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'; // Make sure this line imports from './App', not './components/App'
+import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root'); // Get the root DOM element
-const root = createRoot(rootElement); // Create a root with createRoot
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
